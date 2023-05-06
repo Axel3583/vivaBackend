19882d9b9ac4d725e88ab4a95b5344412b6e0020
Axel Ibo <aibo@aubay.com>
2023-05-06 12:20:14 +0200

    Mise en place de L'exception error.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TicketsModule } from './tickets/tickets.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from '../src/common/filters/http-exception.filter';
 
 @Module({
   imports: [
@@ -10,6 +12,12 @@ import { TicketsModule } from './tickets/tickets.module';
     TicketsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
