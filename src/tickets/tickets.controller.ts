5a696a065d60adf78b16c2cc3bb8bff9a52b123b
Axel Ibo <aibo@aubay.com>
2023-05-06 22:44:47 +0200

    Mise en place de la methode de suppression d'un ticket

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, NotFoundException, Param, Post, HttpException, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, HttpException, HttpStatus } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { Ticket } from '../tickets/entities/ticket.entity';
 
@@ -8,13 +8,18 @@ export class TicketsController {
 
   @Post()
   async createTicket(@Body('code') code: string): Promise<Ticket> {
-    // Vérifiez si le code du ticket ne contient que des caractères alphanumériques
-    const isValidEncoding = /^[a-zA-Z0-9]+$/.test(code);
-
+    // Vérifie si le code du ticket ne contient que des caractères alphanumériques
+    const isValidEncoding = /^[a-zA-Z0-9]{3,}$/.test(code);
+    console.log('isValidEncoding:', isValidEncoding);
     if (!isValidEncoding) {
       throw new HttpException('Ticket code not valid', HttpStatus.BAD_REQUEST);
     }
-
     return await this.ticketsService.createTicket(code);
+  }
+
+  @Delete(':id')
+  async deleteTicket(@Param('id') id: string): Promise<{ message: string }> {
+    await this.ticketsService.deleteTicket(id);
+    return { message: `Ticket with ID ${id} has been deleted` };
   }
 }
