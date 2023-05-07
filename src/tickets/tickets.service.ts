3c489dc13da228dec13893a853b6826186d12374
Axel Ibo <aibo@aubay.com>
2023-05-07 20:57:39 +0200

    vérifie si un ticket avec le même code existe déjà dans la base de données

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Ticket } from '../tickets/entities/ticket.entity';
@@ -11,13 +11,20 @@ export class TicketsService {
   ) { }
 
   async create(createTicketDto: CreateTicketDto): Promise<Ticket> {
+    // Recherche un ticket avec le même code
+    const existingTicket = await this.findByCode(createTicketDto.code);
+    // Si un ticket avec le même code existe déjà, lève une exception
+    if (existingTicket) {
+      throw new HttpException('Ticket with this code already exists', HttpStatus.CONFLICT);
+    }
+    // Sinon, crée et sauvegarde le nouveau ticket
     const newTicket = new this.ticketModel({
       ...createTicketDto,
       createdAt: new Date(),
     });
     return await newTicket.save();
   }
-
+  
   async deleteTicket(id: string): Promise<void> {
     const deletedTicket = await this.ticketModel.findByIdAndDelete(id);
     if (!deletedTicket) {
@@ -28,6 +35,10 @@ export class TicketsService {
   async findAll(): Promise<Ticket[]> {
     return await this.ticketModel.find().exec();
   }
+
+  async findByCode(code: string): Promise<Ticket | null> {
+    return await this.ticketModel.findOne({ code }).exec();
+  }  
 
   async isTicketValid(code: string): Promise<boolean> {
     const ticket = await this.ticketModel.findOne({ code });
